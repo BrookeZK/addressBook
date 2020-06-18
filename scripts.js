bac771c6b6c9fd7b3ea79fec469e317cd6e48c63
Brooke Kullberg <brookekullberg@gmail.com>
2020-06-18 10:48:01 -0700

    add comments to explain refactor to not use global vars

--- scripts.js
+++ scripts.js
@@ -49,7 +49,10 @@ Contact.prototype.fullName = function() {
 }
 
 // User Interface Logic ---------
+// we are no longer instantiating our addressBook instance at the global level. Instead we will instantiate it on line 98. When a variable is globally scoped, any function or method in scripts.js has direct access to it. The problem with global variables is that since they can be accessed anywhere within the document, it becomes very challenging to track which function or line of code is reading from (accessing it) or writing to (assigning a new value) it. 
+// let addressBook = new AddressBook(); // we remove this line
 
+// this function does not have to be updated, because it is already expecting the addressBook object to be passed into it
 function displayContactDetails(addressBookToDisplay) {
   let contactsList = $("ul#contacts");
   let htmlForContactInfo = "";
@@ -59,8 +62,9 @@ function displayContactDetails(addressBookToDisplay) {
   contactsList.html(htmlForContactInfo);
 };
 
-function showContact(contactId, addressBook) {
-  let contact = addressBook.findContact(contactId);
+// in this function we've updated the declaration to include another parameter: 'addressBookParam'. The parameter is the placeholder for the argument that will be passed in when we call the function on line 81
+function showContact(contactId, addressBookParam) {
+  let contact = addressBookParam.findContact(contactId);
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
@@ -71,20 +75,30 @@ function showContact(contactId, addressBook) {
   buttons.append(`<button class='deleteButton' id='${contact.id}'>Delete</button>`);
 }
 
-function attachContactListeners(addressBook) {
+// in this function we've updated the declaration to include a parameter: 'addressBookParam'. The parameter is the placeholder for the argument that will be passed in when we call the function on line 101
+function attachContactListeners(addressBookParam) {
   $("ul#contacts").on("click", "li", function() {
-    showContact(this.className, addressBook);
+    showContact(this.className, addressBookParam);
   });
   $("#buttons").on("click", ".deleteButton", function() {
-    addressBook.deleteContact(this.id); 
+    addressBookParam.deleteContact(this.id); 
     $("#show-contact").hide();
-    displayContactDetails(addressBook); 
+    // this function call needs to be updated to have the argument being passed in to match the attachContactListeners parameter.
+    displayContactDetails(addressBookParam); 
   });
 };
 
 
 $(document).ready(function() {
+  // we are instantiating our addressBook object within the scope of the $(document).ready() event listener. Everything within the opening and closing curly brackets of the $(document).ready() will have access to the 'addressBook' variable.
+  // since 'addressBook' is no longer available everywhere in scripts.js, we need to pass addressBook as an argument to the functions that need access to the object (whether to read its values or update its values). 
+  // To complete this refactor we need to update certain function declarations to have a new parameter, and we need to update certain function calls to pass in a new argument
+  // These are the functions that need to be updated to access to the addressBook object within it's new scope:
+  // attachContactListeners()
+  // showContact()
   let addressBook = new AddressBook();
+
+  // below, we are passing in addressBook as an ARGUMENT
   attachContactListeners(addressBook);
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -96,6 +110,7 @@ $(document).ready(function() {
     $("input#new-phone-number").val("");
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
+    // this function call does not have to be updated, because it is already passing in addressBook as an object
     displayContactDetails(addressBook);
   });
 });
